aff1632c118586dbc89ceaa282193e8bce9c336e
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-14 18:50:10 +0900

    feat: 클래스형 컴포넌트에서 props 사용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 import MyComponent from "./MyComponent";
 
 function App(){
-  return <MyComponent name='React'>리액트</MyComponent>
+  return <MyComponent name='React' favoriteNumber={1}>리액트</MyComponent>
 }
 
 
