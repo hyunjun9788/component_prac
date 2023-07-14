3919bee5bf6ae6370d52c9429ccd49a756e01442
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-14 17:16:37 +0900

    feat:props 사용

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = (props) => {
+    return (
+        <div>
+            안녕하세여, 제 이름은 {props.name}입니다.
+        </div>
+    );
+};
+
+export default MyComponent;
