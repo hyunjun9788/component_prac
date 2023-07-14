7911a653e86dda94ec82be5fd999495e2e8db8ca
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-14 19:30:40 +0900

    feat: this.setState에 함수 인자 전달

--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,38 @@
 import {Component} from 'react';
 
-class Counter extends Component{
+class Counter extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            number:0,
-            fixedNumber:0
+            number: 0,
+            fixedNumber: 0
         };
     }
-    render(){
-        const {number, fixedNumber}=this.state;
-        return(
+
+    render() {
+        const {number, fixedNumber} = this.state;
+        return (
             <div>
                 <h1>{number}</h1>
                 <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
-                    onClick={()=>{
-                    this.setState({number:number+1})
+                    onClick={() => {
+                        this.setState(prevState => {
+                            return {
+                                number: prevState.number + 1
+                            }
+                        })
+                        this.setState(prevState => ({
+                            number: prevState.number + 1
+                        }))
                     }}
-                    >
+                >
                     +1
                 </button>
             </div>
         )
-}}
+    }
+}
 
 export default Counter;
